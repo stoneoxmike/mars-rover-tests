03fea30f9f61f567f982c85abe40babd8d5e287c
coachbahn <38042887+coachbahn@users.noreply.github.com>
2018-04-03 17:51:05 -0400

    Update LaunchDay_Final.ino

--- LaunchDay_Final/LaunchDay_Final.ino
+++ LaunchDay_Final/LaunchDay_Final.ino
@@ -17,53 +17,73 @@
 #define TURRET_TIME 3000
 
 void setup() {
-  initialize();       //Turn everything on                            (In Progress)  
-  detectLaunch();     //Read MMA for launch acceleration signature    (Needs copy/pasted in)
-  delay(60000);       //Read UTS for ground approach signature        (Exact number should be discussed)
-  releaseParachute(); //Move parachute release servo                  (Needs numbers specified)
-  delay(10000);       //Wait to land and settle                       (Exact number can be changed)
-  releaseTank();      //Move tank release servo                       (DONE, needs testing)
-  driveTank();        //Move tank, drop marker, move tank             (DONE, needs testing)
-  captureImages();    //Store 4 images to SD card                     (In Progress)
-  transmitIamges();   //Transfer 4 images via xBee to ground station  (In Progress)
+  initialize();       //Turn everything on                                (In Progress)  
+  detectLaunch(15);   //Read MMA for launch acceleration signature        (DONE, needs final testing)
+  delay(40000);       //Wait to clear separation debris, in milliseconds  (DONE, agree on number)  Sensor read "Out of Range" from T+20 seconds to T+105 seconds during February launch.
+  detectGround(200);  //Read UTS for ground approach signature            (DONE, based on detectLaunch, untested)  (Approach data from Feb: 77, 126, 138, out, out, out, out, out, 312, 275, 246, 220, 191, 160, 133, 110, 91, 67, 48, 16, 10)
+  releaseParachute(); //Move parachute release servo                      (Needs numbers specified)
+  delay(10000);       //Wait to land and settle, in milliseconds          (DONE)
+  releaseTank();      //Move tank release servo                           (DONE, needs testing)
+  driveTank();        //Move tank, drop marker, move tank                 (DONE, needs testing)
+  captureImages();    //Store 4 images to SD card                         (In Progress)
+  transmitIamges();   //Transfer 4 images via xBee to ground station      (In Progress)
 }
 
+
 void initialize(){
-  Servo parachute_servo;        //Initialize Parachute Servo
+  Servo parachute_servo;        //Initialize PARACHUTE Servo
   parachute_servo.attach(9);    //Use pin 9
   parachute_servo.write(lockdegrees);       //NEEDS A SPECIFIC NUMBER
   
-  Servo tankRelease_servo;      //Initialize Tank Release Servo
+  Servo tankRelease_servo;      //Initialize TANK Release Servo
   tankRelease_servo.attach(10);   //Use pin 10
   tankRelease_servo.write(100);
   
-  Adafruit_MMA8451 mma = Adafruit_MMA8451();    //Initialize accelerometer and name it "MMA"
+  Adafruit_MMA8451 mma = Adafruit_MMA8451();    //Initialize ACCELEROMETER and name it "MMA"
   
   SR04 sr04 = SR04(6, 7);          //Initialize UTS and name it "sr04"  (Echo_pin = 6, Trig_pin = 7)
   
-  SD.begin(53);                   //Initialize the SD card reader using pin 53 for CS
+  SD.begin(53);                   //Initialize the SD CARD READER using pin 53 for CS
   
   SoftwareSerial cam1SerialConnection = SoftwareSerial(63, 62);   //Initialize Camera 1
-  Adafruit_VC0706 cam1 = Adafruit_VC0706(&cam1SerialConnection);                            //and name it "cam1"
+  Adafruit_VC0706 cam1 = Adafruit_VC0706(&cam1SerialConnection);  //and name it "cam1"
   
   SoftwareSerial cam2SerialConnection = SoftwareSerial(65, 64);   //Initialize Camera 2
-  Adafruit_VC0706 cam2 = Adafruit_VC0706(&cam2SerialConnection);                            //and name it "cam2"
+  Adafruit_VC0706 cam2 = Adafruit_VC0706(&cam2SerialConnection);  //and name it "cam2"
   
   SoftwareSerial cam3SerialConnection = SoftwareSerial(67, 66);   //Initialize Camera 3
-  Adafruit_VC0706 cam3 = Adafruit_VC0706(&cam3SerialConnection);                            //and name it "cam3"
+  Adafruit_VC0706 cam3 = Adafruit_VC0706(&cam3SerialConnection);  //and name it "cam3"
 
   SoftwareSerial cam4SerialConnection = SoftwareSerial(69, 68);   //Initialize Camera 4
-  Adafruit_VC0706 cam4 = Adafruit_VC0706(&cam4SerialConnection);                            //and name it "cam4"
+  Adafruit_VC0706 cam4 = Adafruit_VC0706(&cam4SerialConnection);  //and name it "cam4"
   
   Serial1.begin(9600);    //Initialize xBee Serial Connection @ 9600 bps
 }
 
-void detectLaunch(){
-
+void detectLaunch(float threshold){             //Threshld for detection is in m/s^2
+  int i = 0;
+  bool detected = false;
+  while(!detected) {
+    sensors_event_t event;                      //Magical MMA Gobblety-Gook
+    mma.getEvent(&event);                       //Magical MMA Gobblety-Gook
+    if(event.acceleration.x > threshold) i++;   //Check if we are above the threshold for detection, if so, add to count
+      else i = 0;                               //If the reading is not above the threshold, reest the counter.  (Thanks, Geyer)
+    if(i >= 4) detected = true;                 //If the counter reaches 4, we have launched
+    delay(100);                                 //Even Accelerometers needs a break sometimes
+    }
+  //if(detected == true) Serial1.println("DETECTED!");      //Commented out for now... Can add back in for debug...
 }
 
-void detectGround(){
-
+void detectGround(float threshold){         //Threshold for detection is in cm.
+  int i = 0;
+  bool detected = false;
+  while(!detected) {
+    if(sr04.Distance() < threshold) i++;    //I think that when this is "Out of Range" it reports "0".  If so, we need top address that.  We'll check it tomorrow in class.
+      else i = 0;
+    if(i >= 4) detected = true;
+    delay(100);
+    }
+  //if(detected == true) Serial1.println("GROUND DETECTED!");  //Commented out for now... Can add back in for debug...
 }
 
 void releaseParachute(){
