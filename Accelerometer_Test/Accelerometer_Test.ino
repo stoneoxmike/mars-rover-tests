e3fb371e008f6f89d498b2498f8221b8360d0a38
stoneoxmike <stoneoxmike@gmail.com>
2018-04-03 11:26:17 -0400

    Third commit

--- Accelerometer_Test/Accelerometer_Test.ino
+++ Accelerometer_Test/Accelerometer_Test.ino
@@ -43,54 +43,7 @@ void setup(void) {
 }
 
 void loop() {
-  // Read the 'raw' data in 14-bit counts
-  //mma.read();
-  //Serial1.print("X:\t"); Serial1.print(mma.x); 
-  //Serial1.print("\tY:\t"); Serial1.print(mma.y); 
-  //Serial1.print("\tZ:\t"); Serial1.print(mma.z); 
-  //Serial1.println();
-
-  /* Get a new sensor event */ 
-  //sensors_event_t event; 
-  //mma.getEvent(&event);
-
-  /* Display the results (acceleration is measured in m/s^2) */
-  //Serial1.print("X: \t"); Serial1.print(event.acceleration.x); Serial1.print("\t");
-  //Serial1.print("Y: \t"); Serial1.print(event.acceleration.y); Serial1.print("\t");
-  //Serial1.print("Z: \t"); Serial1.print(event.acceleration.z); Serial1.print("\t");
-  //Serial1.println("m/s^2 ");
-  
-  /* Get the orientation of the sensor */
-  //uint8_t o = mma.getOrientation();
-  /*
-  switch (o) {
-    case MMA8451_PL_PUF: 
-      Serial.println("Portrait Up Front");
-      break;
-    case MMA8451_PL_PUB: 
-      Serial.println("Portrait Up Back");
-      break;    
-    case MMA8451_PL_PDF: 
-      Serial.println("Portrait Down Front");
-      break;
-    case MMA8451_PL_PDB: 
-      Serial.println("Portrait Down Back");
-      break;
-    case MMA8451_PL_LRF: 
-      Serial.println("Landscape Right Front");
-      break;
-    case MMA8451_PL_LRB: 
-      Serial.println("Landscape Right Back");
-      break;
-    case MMA8451_PL_LLF: 
-      Serial.println("Landscape Left Front");
-      break;
-    case MMA8451_PL_LLB: 
-      Serial.println("Landscape Left Back");
-      break;
-    }*/
   wait_for_acceleration(1.5);
-  
 }
 
 void wait_for_acceleration(float threshold) {
