8c4a751edd85c6f11ce4b63225fe87b00a951add
stoneoxmike <stoneoxmike@gmail.com>
2018-04-04 09:02:44 -0400

    Update LaunchDay_Final.ino

--- LaunchDay_Final/LaunchDay_Final.ino
+++ LaunchDay_Final/LaunchDay_Final.ino
@@ -16,50 +16,47 @@
 #define SPIN_TIME 500
 #define TURRET_TIME 3000
 
+Servo parachute_servo;        //Initialize PARACHUTE Servo
+parachute_servo.attach(9);    //Use pin 9
+parachute_servo.write(45);       //NEEDS A SPECIFIC NUMBER
+  
+Servo tankRelease_servo;      //Initialize TANK Release Servo
+tankRelease_servo.attach(10);   //Use pin 10
+tankRelease_servo.write(100);
+  
+Adafruit_MMA8451 mma = Adafruit_MMA8451();    //Initialize ACCELEROMETER and name it "MMA"
+mma.begin();                                   //Begin comminucation with accelerometer
+mma.setRange(MMA8451_RANGE_4_G);              //NOTE: This had been set to 2_G, I recommend changing to 4_G and setting the detection threshold to >20 m/s^2.  There are instances in the data from February that would have caused a pre-mature detection.
+  
+SR04 sr04 = SR04(6, 7);          //Initialize UTS and name it "sr04"  (Echo_pin = 6, Trig_pin = 7)
+  
+pinMode(53, OUTPUT);            //Set the CS Pin as output
+SD.begin(53);                   //Initialize the SD CARD READER using pin 53 for CS
+  
+SoftwareSerial cam1SerialConnection = SoftwareSerial(63, 62);   //Initialize Camera 1
+Adafruit_VC0706 cam1 = Adafruit_VC0706(&cam1SerialConnection);  //and name it "cam1"
+  
+SoftwareSerial cam2SerialConnection = SoftwareSerial(65, 64);   //Initialize Camera 2
+Adafruit_VC0706 cam2 = Adafruit_VC0706(&cam2SerialConnection);  //and name it "cam2"
+  
+SoftwareSerial cam3SerialConnection = SoftwareSerial(67, 66);   //Initialize Camera 3
+Adafruit_VC0706 cam3 = Adafruit_VC0706(&cam3SerialConnection);  //and name it "cam3"
+
+SoftwareSerial cam4SerialConnection = SoftwareSerial(69, 68);   //Initialize Camera 4
+Adafruit_VC0706 cam4 = Adafruit_VC0706(&cam4SerialConnection);  //and name it "cam4"
+  
+Serial1.begin(9600);    //Initialize xBee Serial Connection @ 9600 bps
+
 void setup() {
-  initialize();       //Turn everything on                                (In Progress)  
   detectLaunch(15);   //Read MMA for launch acceleration signature        (DONE, needs final testing)  Note: We should try changing the range to mma.setRange(MMA8451_RANGE_4_G); and readig some data.  Then we can detect a larger acceleration and rule out more noise. Or, make it 6 - 8 consecutive readings.
   delay(40000);       //Wait to clear separation debris, in milliseconds  (DONE, agree on number)  Sensor read "Out of Range" from T+20 seconds to T+105 seconds during February launch.
   detectGround(200);  //Read UTS for ground approach signature            (DONE, based on detectLaunch, untested)  (Approach data from Feb: 77, 126, 138, out, out, out, out, out, 312, 275, 246, 220, 191, 160, 133, 110, 91, 67, 48, 16, 10)
-  releaseParachute(); //Move parachute release servo                      (Needs numbers specified)
+  parachute_servo.write(120); //Move parachute release servo                      (Needs numbers specified)
   delay(10000);       //Wait to land and settle, in milliseconds          (DONE)
-  releaseTank();      //Move tank release servo                           (DONE, needs testing)
+  tankRelease_servo.write(10);      //Move tank release servo                           (DONE, needs testing)
   driveTank();        //Move tank, drop marker, move tank                 (DONE, needs testing)
   captureImages();    //Store 4 images to SD card                         (In Progress)
   transmitIamges();   //Transfer 4 images via xBee to ground station      (In Progress)
-}
-
-
-void initialize(){
-  Servo parachute_servo;        //Initialize PARACHUTE Servo
-  parachute_servo.attach(9);    //Use pin 9
-  parachute_servo.write(45);       //NEEDS A SPECIFIC NUMBER
-  
-  Servo tankRelease_servo;      //Initialize TANK Release Servo
-  tankRelease_servo.attach(10);   //Use pin 10
-  tankRelease_servo.write(100);
-  
-  Adafruit_MMA8451 mma = Adafruit_MMA8451();    //Initialize ACCELEROMETER and name it "MMA"
-  mma.begin();                                   //Begin comminucation with accelerometer
-  mma.setRange(MMA8451_RANGE_4_G);              //NOTE: This had been set to 2_G, I recommend changing to 4_G and setting the detection threshold to >20 m/s^2.  There are instances in the data from February that would have caused a pre-mature detection.
-  
-  SR04 sr04 = SR04(6, 7);          //Initialize UTS and name it "sr04"  (Echo_pin = 6, Trig_pin = 7)
-  
-  SD.begin(53);                   //Initialize the SD CARD READER using pin 53 for CS
-  
-  SoftwareSerial cam1SerialConnection = SoftwareSerial(63, 62);   //Initialize Camera 1
-  Adafruit_VC0706 cam1 = Adafruit_VC0706(&cam1SerialConnection);  //and name it "cam1"
-  
-  SoftwareSerial cam2SerialConnection = SoftwareSerial(65, 64);   //Initialize Camera 2
-  Adafruit_VC0706 cam2 = Adafruit_VC0706(&cam2SerialConnection);  //and name it "cam2"
-  
-  SoftwareSerial cam3SerialConnection = SoftwareSerial(67, 66);   //Initialize Camera 3
-  Adafruit_VC0706 cam3 = Adafruit_VC0706(&cam3SerialConnection);  //and name it "cam3"
-
-  SoftwareSerial cam4SerialConnection = SoftwareSerial(69, 68);   //Initialize Camera 4
-  Adafruit_VC0706 cam4 = Adafruit_VC0706(&cam4SerialConnection);  //and name it "cam4"
-  
-  Serial1.begin(9600);    //Initialize xBee Serial Connection @ 9600 bps
 }
 
 void detectLaunch(float threshold){             //Threshld for detection is in m/s^2
@@ -86,14 +83,6 @@ void detectGround(float threshold){         //Threshold for detection is in cm.
     delay(100);
     }
   //if(detected == true) Serial1.println("GROUND DETECTED!");  //Commented out for now... Can add back in for debug...
-}
-
-void releaseParachute(){
-  parachute_servo.write(120);
-}
-
-void releaseTank(){
-  tankRelease_servo.write(10);
 }
 
 void driveTank(){          //Three variables are DEFINED at the top of the program because they are some of the few things that WILL be changed on launch day
